7d6d90ac74c3c069113285a25a3c7b9b0bc7fda4
Ariq Ramdhany <ariqathallah38@gmail.com>
2024-10-16 17:26:53 +0900

    fixed copydatabase and search item layout

--- app/src/main/java/com/example/namapopup/DBHelper.java
+++ app/src/main/java/com/example/namapopup/DBHelper.java
@@ -16,6 +16,7 @@ import android.util.Log;
 
 import androidx.compose.material3.AlertDialogDefaults;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -47,13 +48,7 @@ public class DBHelper extends SQLiteOpenHelper {
         }
 
 
-        // Check if the database exists, if not, copy it from assets
-//        try {
-//            copyDatabaseFromAssets();
-//        } catch (IOException e) {
-//            throw new Error("Error copying database from assets", e);
-//        }
-//        addFoundColumnToAllRegions();
+
     }
 
     private void copyDatabaseFromAssets() throws IOException {
@@ -76,7 +71,13 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String ken = "ken\"s birthday";
-        // We don't need to create the database here as we're copying it from assets
+        // Check if the database exists, if not, copy it from assets
+        try {
+            copyDatabaseFromAssets();
+        } catch (IOException e) {
+            throw new Error("Error copying database from assets", e);
+        }
+        addFoundColumnToAllRegions();
     }
 
     @Override
